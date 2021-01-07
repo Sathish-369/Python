2f971fad65a6d1b1d56f42366a0e999b8281bedd
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-01-07 19:59:24 +0200

    Update Basic python program7.py

--- Basic python program7.py
+++ Basic python program7.py
@@ -15,13 +15,18 @@ while True:
     if name in user:
       print("Welcome Mr.{}".format(name))
 
-      remove=(input("enter employee name who is not here (y/n)?:  "))
+      remove=(input("Would you like to remove above name (y/n)?:  "))
       if(remove == "y"):
           user.remove(name)
       else:
-          print("no problem I didn't want you to leave")
-
-
+          print("No problem I didn't want you to leave")
       print(user)
     else:
       print("Not valid name")
+      add=input("Would you like to add your name in the system (y/n)?:  ")
+
+      if(add =="y"):
+          user.append(name)
+          print(user)
+      else:
+          print("See you later")
